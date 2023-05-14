4c9903c71e258d622f0d863568b000b04b9ecd7a
OddEer0 <OddEer0@gmail.com>
2023-05-14 20:46:05 +0600

    folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import * as path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src/"),
+			public: `${path.resolve(__dirname, "./public/")}`,
+			app: `${path.resolve(__dirname, "./src/app/")}`,
+			processes: `${path.resolve(__dirname, "./src/processes/")}`,
+			pages: `${path.resolve(__dirname, "./src/pages/")}`,
+			widgets: `${path.resolve(__dirname, "./src/widgets/")}`,
+			features: `${path.resolve(__dirname, "./src/features/")}`,
+			entities: `${path.resolve(__dirname, "./src/entities/")}`,
+			shared: `${path.resolve(__dirname, "./src/shared/")}`
+		}
+	}
 })
